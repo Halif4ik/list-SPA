2008404faa89d0cda41f2aa6e77c8fbf4392c38b
DimaGrimach <chiki.briki2@gmail.com>
2023-08-21 16:34:02 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express,{Request,Response} from 'express';
+const exprApp = express()
+const port = process.env.PORT || 3001
+import path from'path';
+import bodyParser from "body-parser";
+
+exprApp.use(bodyParser({}));
+
+exprApp.get('/', (req:Request, res:Response) => {
+    res.sendFile(path.join(__dirname, '../views','indexSpp.html'))
+});
+
+
+exprApp.listen(port, () => {
+    console.log(`Example MYapp listening on port ${port}`)
+})
