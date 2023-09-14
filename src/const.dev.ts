ac2fca1f7cbf75a11f8d0e4c82de15746a9bd916
DimaGrimach <chiki.briki2@gmail.com>
2023-09-14 14:48:52 +0300

    const

--- src/const.dev.ts
+++
@@ -1,7 +0,0 @@
-module.exports={
-    mongoURL: 'mongodb+srv://chikibriki2:KQoDNhuWCigEleSS@cluster1.vhd57va.mongodb.net/Exchange_of_developers',
-    SEND_GRID_API_KEY: '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',
-    HOST_EMAIL: 'chiki-briki2@ya.ru',
-    BASE_URL: 'http://grymachdmitro.ddns.net/',
-    PAGE_PAGINATION: 4,
-}
